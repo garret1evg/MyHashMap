ec71c1dea1eb8eabe9e620a657de8c4107b3abec
Evgenii Chmutov <garret.evg@gmail.com>
2020-07-24 15:57:45 +0300

    finish

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -1,3 +1,6 @@
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 /*******************************************************************************
  * Copyright (c) 2020 Chmutov Eugene <garret.evg@gmail.com>.
  * All rights reserved.
@@ -5,7 +8,7 @@
 
 public class MyHashMap {
 
-    private final int DEFAULT_INITIAL_CAPACITY = 37;//размер карты по умолчанию
+    private final int DEFAULT_INITIAL_CAPACITY = 16;//размер карты по умолчанию
     private final float LOAD_FACTOR = 0.5f;//максимальная загруженость карты по умолчанию
     private final float MULTIPLIER = 2.0f; //во столько раз увеличится размер карты при пересборке по умолчанию
 
@@ -53,23 +56,71 @@ public class MyHashMap {
         }
     }
 
+    // кладет элемент с ключом и значением
     public boolean put(int key, long value) {
+        if (size + 1 >= capacity * loadFactor) {
+            resize();
+        }
+        int index = getIndex(key);
+        for (int i = index; ; i++) {
+            if(i == capacity) i = 0;
+            if(table[i] == null) {
+                table[i] = new Entry(key,value);
+                size++;
+                return true;
+            }
+            if (table[i].getKey() == key){
+                if(table[i].getValue() == value)
+                    return true;
+                throw new RuntimeException("Key is already is use!!!");
+            }
 
+        }
     }
 
-
+    // возвращает значение по ключу
     public long get(int key) {
-
+        for (int i = getIndex(key); ; i++) {
+            if(i == capacity) i = 0;
+            if(table[i] == null) throw new NoSuchElementException("No such key! -> [" + key + "]");
+            if (table[i].getKey() == key) return table[i].getValue();
+        }
     }
 
-
+    // возвращает количество элементов в карте
     public int size() {
-
+        return size;
     }
 
-    public static void main(String[] args){
+    // увеличение размера карты
+    private void resize(){
+        capacity*=multiplier;
+        Entry[] oldTable = table;
+        table = new Entry[capacity];
+        size = 0;
 
+        for(Entry e : oldTable)
+            if(e != null)
+                put(e.getKey(),e.getValue());
+    }
 
+    // возвращает номер позиции по значению хэш-функции
+    private int getIndex(int key){
+        return hash(key) % capacity;
+    }
+
+    // хэш-функция
+    private int hash(int hash){
+        if (hash == 0) return 0;
+        return Objects.hashCode(hash);
+    }
+
+    public boolean containsKey(int key){
+        for (int i = getIndex(key); ; i++) {
+            if(i == capacity) i = 0;
+            if(table[i] == null) return false;
+            if (table[i].getKey() == key) return true;
+        }
     }
 
     //Пара <ключ, значение>
--- src/test/java/MyHashMapTest.java
+++ src/test/java/MyHashMapTest.java
@@ -1,2 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Chmutov Eugene <garret.evg@gmail.com>.
+ * All rights reserved.
+ ******************************************************************************/
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class MyHashMapTest {
+    MyHashMap map = new MyHashMap();
+    @Before
+    public void setUp() throws Exception {
+        map = new MyHashMap();
+        map.put(32, 1L);
+        map.put(22, 2L);
+        map.put(15, 3L);
+        map.put(20, 5L);
+
+    }
+    @Test
+    public void putTest() throws Exception {
+        assertTrue(map.put(27, 6L));
+    }
+
+    @Test
+    public void getTest() throws Exception {
+        long value = map.get(15);
+        assertEquals(3L, value);
+
+    }
+
+    @Test
+    public void sizeTest() throws Exception {
+        int size = map.size();
+        assertEquals(4, size);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getWrongKeyTest() throws Exception {
+        map.get(100);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorTest() throws Exception {
+        MyHashMap map1 = new MyHashMap(-1);
+    }
+    @Test
+    public void resizeTest() throws Exception {
+        MyHashMap map1 = new MyHashMap(8,0.5f,2f);
+        map1.put(32, 1L);
+        map1.put(22, 2L);
+        map1.put(15, 3L);
+        map1.put(20, 5L);
+        map1.put(27, 6L);
+        assertEquals(5, map1.size());
+        assertEquals(1L, map1.get(32));
+        assertEquals(2L, map1.get(22));
+        assertEquals(3L, map1.get(15));
+        assertEquals(5L, map1.get(20));
+        assertEquals(6L, map1.get(27));
+    }
+    @Test
+    public void putByKeyInUseWithSameValueTest() throws Exception {
+        int prevSize = map.size();
+        map.put(20, 5L);
+        assertEquals(prevSize, map.size());
+        assertEquals(5L, map.get(20));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void putByKeyInUseWithAnotherValueTest() throws Exception {
+        map.put(20, 7L);
+    }
+
+    @Test
+    public void randomStressTest(){
+        int numberEntries = 30000000;
+        Random rand = new Random();
+        int key;
+        long value;
+        int[] keys = new int[numberEntries];
+        long[] values = new long[numberEntries];
+        int currSize = map.size();
+        for (int i=0;i<numberEntries;i++){
+            key = rand.nextInt(100000);
+            value = rand.nextLong();
+            if(!map.containsKey(key)){
+                map.put(key,value);
+                keys[i] = key;
+                values[i] = value;
+                assertEquals(++currSize, map.size());
+                assertEquals(value, map.get(key));
+            }
+
+        }
+        for (int i=0;i<numberEntries;i++){
+            if(keys[i]!=0){
+                assertEquals(values[i], map.get(keys[i]));
+            }
+        }
+    }
+
 }
